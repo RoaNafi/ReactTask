0967501e2411b3b2c2320b29cbcdde664210c67c
RoaNafi <1201959@student.birzeit.edu>
2024-08-05 17:51:06 +0300

    hi

--- src/components/PricingSection.js
+++ src/components/PricingSection.js
@@ -3,7 +3,8 @@ import '../styles/PricingSection.css';
 
 const PricingSection = () => {
   return (
-    <section className="pricing-section">
+    <section className="pricing-section
+    ">
       <div className="pricing-content">
         <h2>A Price To Suit Everyone</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
@@ -11,7 +12,8 @@ const PricingSection = () => {
           <h3>$40</h3>
           <span className="kit">UI Design Kit</span>
         </div>
-        <span className="purchase-par">See, One price. Simple.</span>
+        <span 
+        className="purchase-par">See, One price. Simple.</span>
         <button className="purchase-button">Purchase Now</button>
       </div>
     </section>
